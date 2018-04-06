d440948616701b11254855111dcdd902329880d9
aspirin37 <molir10@mail.ru>
2018-04-06 16:51:06 +0300

    MST-3001 sign fixes

--- src/components/sign/SignupBlock.vue
+++ src/components/sign/SignupBlock.vue
@@ -1,33 +1,14 @@
 <template>
   <div class="w-100">
-    <form v-on:submit.prevent="validateBeforeSubmit" v-if="!smsSended" class="d-inline-block">
+
+    <!-- Phone -->
+    <div class="d-inline-block" v-if="!smsSended">
       <p class="lead mw-600 text-left" v-if="signText">{{signText}}</p>
-      <div class="d-inline-flex flex-wrap flex-md-nowrap text-left">
-        <div class="input-group mr-3 w-auto flex-md-nowrap">
-          <input
-            v-masked-input="currentPhoneMask[`${globPhone ? 1 : 0}`]"
-            type="tel"
-            name="phone"
-            v-model="phone"
-            class="form-control form-control-lg sign-input-width mx-0 mb-3"
-            placeholder="Телефон"
-            v-validate="'required'"
-            v-bind:class="{'border-danger' : errors.has('phone'), 'bg-white border-secondary' : !errors.has('phone') && formatedPhone.length > 10}"
-          >
-          <div class="input-group-append mb-3">
-            <button type="submit" class="btn btn-lg btn-theme w-100 sign-mw px-2 px-sm-3">Получить код</button>
-          </div>
-        </div>
-        <div class="mb-3 d-inline-block d-flex align-items-center w-100">
-          <a href="#" v-on:click.prevent="$emit('toggleSign')" class="btn btn-outline-secondary mr-2 btn-lg"><img src="../../assets/icons/login.svg" class="mr-1 vertical-middle half-opacity" alt=""><b>Войти</b></a>
-          <small class="text-muted">Если уже пользовались<br>сервисом</small>
-        </div>
-      </div>
-      <p class="text-danger text-left" v-if="errorMessage">{{errorMessage}}</p>
-      <p class="small text-muted mw-400 text-left mr-auto">
-        Используя сервис, вы соглашаетесь с <a href="#" v-on:click.prevent="toggleModal('terms')" target="_blank" class="text-dark">пользовательским соглашением</a> и обработкой <a href="#" v-on:click.prevent="toggleModal('privacy')" target="_blank" class="text-dark">персональных данных.</a>
-      </p>
-    </form>
+      <signup-phone-form v-on:submitted="sendSms(phone = $event)" :phone="phone"></signup-phone-form>
+    </div>
+    <!-- /Phone -->
+
+    <!-- Pin -->
     <form v-on:submit.prevent="confirmAccount" v-else class="d-inline-block text-left">
       <p class="lead mw-600 mb-0">Мы отправили СМС с кодом на номер {{phone}}.</p>
       <p class="lead mw-600">{{pinText}}</p>
@@ -48,76 +29,22 @@
             <button type="submit" class="btn btn-lg btn-theme w-100">Подтвердить</button>
           </div>
         </div>
-        <!-- <div class="form-group">
-          <input
-            v-masked-input="currentPhoneMask[`${globPhone ? 1 : 0}`]"
-            type="tel"
-            name="phone"
-            v-model="phone"
-            class="form-control sign-mw mx-auto"
-            placeholder="Телефон"
-            v-validate="'required'"
-            v-bind:class="{'border-danger' : errors.has('phone'), 'border-success' : !errors.has('phone') && formatedPhone.length > 10}"
-          >
-        </div>
-        <div class="form-group text-center">
-          <button type="submit" class="btn btn-theme w-100">Подтвердить</button>
-        </div> -->
         <div class="mb-3 d-inline-block d-flex align-items-center w-100">
           <a href="#" v-on:click.prevent="$emit('toggleSign')" class="btn btn-outline-secondary mr-2 btn-lg"><img src="../../assets/icons/login.svg" class="mr-1 vertical-middle half-opacity" alt=""><b>Войти</b></a>
           <small class="text-muted">Если уже пользовались<br>сервисом</small>
         </div>
       </div>
-
-      <!-- <div class="form-group sign-mw mx-auto">
-        <input
-          type="text"
-          name="pin"
-          v-model="pin"
-          class="form-control sign-mw"
-          placeholder="Код из СМС"
-          v-validate="'required'"
-          v-bind:class="{'border-danger' : errors.has('pin')}"
-        >
-      </div>
-      <div class="form-group sign-mw mx-auto">
-        <button type="submit" class="btn btn-theme w-100">Подтвердить</button>
-      </div> -->
-      <p class="text-danger" v-if="errorMessage">{{errorMessage}}</p>
     </form>
-    <!-- <new-password v-if="smsSended" :phone="phone" :fromAuth="true" :route="routeTo" v-on:signed="signed"></new-password> -->
-    <modal
-      modalSize="modal-lg"
-      ref="terms"
-      :clickedBack="true"
-    >
-      <div slot="modalBody">
-        <div class="modal-scroll-body modal-scroll-body--md">
-          <div v-html="rules" class="rules-pre"></div>
-        </div>
-      </div>
-    </modal>
-    <modal
-      modalSize="modal-lg"
-      ref="privacy"
-      :clickedBack="true"
-    >
-      <div slot="modalBody">
-        <div class="modal-scroll-body modal-scroll-body--md">
-          <privacy></privacy>
-        </div>
-      </div>
-    </modal>
+    <!-- /Pin -->
+
+    <p class="text-danger" v-if="errorMessage">{{errorMessage}}</p>
+
   </div>
 </template>
 
 <script>
 import auth from '../../auth'
-import api from '../../store/api'
-// import NewPassword from './NewPassword'
-import maskedInput from '../../directives/maskedInput'
-import Modal from '../utils/Modal'
-import Privacy from '../Privacy'
+import SignupPhoneForm from './SignupPhoneForm'
 
 export default {
   name: 'signup-block',
@@ -140,36 +67,18 @@ export default {
       type: String,
       default: ''
     },
-    globName: {
-      type: String,
-      default: ''
-    },
     signText: '',
     pinText: ''
   },
   components: {
-    // NewPassword,
-    Modal,
-    Privacy
-  },
-  directives: {
-    maskedInput
+    SignupPhoneForm
   },
   computed: {
-    currentPhoneMask () {
-      return this.$store.state.phoneMasks[this.$store.state.currentCountry]
-    },
     formatedPhone () {
       return this.phone.replace(/\+|-|_|\(|\)|\s/g, '')
     }
   },
-  mounted () {
-    this.loadrules()
-  },
   methods: {
-    toggleModal (id) {
-      this.$refs[id].newIsOpen = !this.$refs[id].newIsOpen
-    },
     validateBeforeSubmit () {
       this.$validator.validateAll().then(success => {
         if (!success) {
@@ -177,20 +86,6 @@ export default {
         }
         this.sendSms()
       })
-    },
-    openTermsModal (e) {
-      if (!e.ctrlKey) {
-        this.toggleModal('terms')
-      } else {
-        this.$router.push({ path: 'agreement' })
-      }
-    },
-    openPrivacyModal (e) {
-      if (!e.ctrlKey) {
-        this.toggleModal('privacy')
-      } else {
-        this.$router.push({ path: 'privacy' })
-      }
     },
     sendSms () {
       // let tzOffset = -1 * (new Date().getTimezoneOffset() / 60)
@@ -209,14 +104,6 @@ export default {
         pin: this.pin
       }
       auth.signup(this, options, this.routeTo)
-    },
-    loadrules () {
-      this.$http.get(api.API_LINK + '/webview/terms/1/agreement-client.php').then((response) => {
-        this.rules = response.body
-      })
-    },
-    signed () {
-      this.$emit('signed')
     }
   }
 }
---
+++ src/components/sign/SignupPhoneForm.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="validateBeforeSubmit">
+      <div class="d-inline-flex flex-wrap flex-md-nowrap text-left">
+        <div class="input-group mr-3 w-auto flex-md-nowrap">
+          <input
+            v-masked-input="currentPhoneMask[0]"
+            type="tel"
+            name="phone"
+            v-model="thisPhone"
+            class="form-control form-control-lg sign-input-width mx-0 mb-3"
+            placeholder="Телефон"
+            v-bind:class="errors.has('phone') ? 'border-danger' : ''"
+            v-validate="'required|verify_phone'"
+          >
+          <div class="input-group-append mb-3">
+            <button type="submit" class="btn btn-lg btn-theme w-100 sign-mw px-2 px-sm-3">Получить код</button>
+          </div>
+        </div>
+        <div class="mb-3 d-inline-block d-flex align-items-center w-100">
+          <a href="#" v-on:click.prevent="$emit('toggleSign')" class="btn btn-outline-secondary mr-2 btn-lg"><img src="../../assets/icons/login.svg" class="mr-1 vertical-middle half-opacity" alt=""><b>Войти</b></a>
+          <small class="text-muted">Если уже пользовались<br>сервисом</small>
+        </div>
+      </div>
+      <p class="small text-muted mw-400 text-left mr-auto">
+        Используя сервис, вы соглашаетесь с <a href="#" v-on:click.prevent="toggleModal('terms')" target="_blank" class="text-dark">пользовательским соглашением</a> и обработкой <a href="#" v-on:click.prevent="toggleModal('privacy')" target="_blank" class="text-dark">персональных данных.</a>
+      </p>
+    </form>
+
+    <!-- Terms -->
+    <modal
+      modalSize="modal-lg"
+      ref="terms"
+      :clickedBack="true"
+    >
+      <div slot="modalBody">
+        <div class="modal-scroll-body modal-scroll-body--md">
+          <div v-html="rules" class="rules-pre"></div>
+        </div>
+      </div>
+    </modal>
+    <modal
+      modalSize="modal-lg"
+      ref="privacy"
+      :clickedBack="true"
+    >
+      <div slot="modalBody">
+        <div class="modal-scroll-body modal-scroll-body--md">
+          <privacy></privacy>
+        </div>
+      </div>
+    </modal>
+    <!-- /Terms -->
+
+  </div>
+</template>
+
+<script>
+import api from '../../store/api'
+import maskedInput from '../../directives/maskedInput'
+import Modal from '../utils/Modal'
+import Privacy from '../Privacy'
+
+export default {
+  name: 'signup-phone-form',
+  data () {
+    return {
+      rules: '',
+      thisPhone: this.phone,
+      validType: ''
+    }
+  },
+  props: {
+    phone: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    // NewPassword,
+    Modal,
+    Privacy
+  },
+  created () {
+    this.$validator.extend('verify_phone', {
+      getMessage: field => `The ${field} is not a valid.`,
+      validate: value => new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            valid: this.formatedPhone.length >= 11
+          })
+        }, 100)
+      })
+    })
+  },
+  directives: {
+    maskedInput
+  },
+  computed: {
+    currentPhoneMask () {
+      return this.$store.state.phoneMasks[this.$store.state.currentCountry]
+    },
+    formatedPhone () {
+      return this.thisPhone.replace(/\+|-|_|\(|\)|\s/g, '')
+    }
+  },
+  mounted () {
+    this.loadrules()
+  },
+  methods: {
+    toggleModal (id) {
+      this.$refs[id].newIsOpen = !this.$refs[id].newIsOpen
+    },
+    validateBeforeSubmit () {
+      this.$validator.validateAll().then(success => {
+        if (!success) {
+          return
+        }
+        this.$emit('submitted', this.thisPhone)
+      })
+    },
+    openTermsModal (e) {
+      if (!e.ctrlKey) {
+        this.toggleModal('terms')
+      } else {
+        this.$router.push({ path: 'agreement' })
+      }
+    },
+    openPrivacyModal (e) {
+      if (!e.ctrlKey) {
+        this.toggleModal('privacy')
+      } else {
+        this.$router.push({ path: 'privacy' })
+      }
+    },
+    loadrules () {
+      this.$http.get(api.API_LINK + '/webview/terms/1/agreement-client.php').then((response) => {
+        this.rules = response.body
+      })
+    }
+  }
+}
+</script>
