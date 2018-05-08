9a1a00b13cb1db9e3b777a99bbb12a7b1e3eb6c2
aspirin37 <molir10@mail.ru>
2018-05-08 17:26:40 +0300

    MST-2931 confidential layout

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
+  SITE_URL: '"https://mesto.a3technology.ru/"',
   NODE_ENV: '"development"',
   API_URL: '"https://api.a3technology.ru/"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
+  SITE_URL: '"https://mesto.delivery/"',
   NODE_ENV: '"production"',
   API_URL: '"https://api.mesto.delivery/"'
 }
