04ff992ea47e4811e982235ea23b6829d9af70e8
aspirin37 <molir10@mail.ru>
2018-05-10 11:22:20 +0300

    urls

--- src/pages/Confidential.vue
+++ src/pages/Confidential.vue
@@ -17,7 +17,9 @@ export default {
   },
   methods: {
     getAreement () {
-      this.$http.get('https://mesto.a3technology.ru/terms/confidential/').then(response => {
+      let url = `${window.location.origin}/terms/confidential/`
+
+      this.$http.get(url).then(response => {
         this.confidential = response.body
       })
       // this.$http.get(api.API_LINK + '/webview/terms/actual/confidential.php').then(response => {
