7c29c4dd1ec5929a9173a934c5b1ef232084d1f5
aspirin37 <molir10@mail.ru>
2018-05-23 15:23:58 +0300

    MST-2234 active card

--- src/components/payment/CardItem.vue
+++ src/components/payment/CardItem.vue
@@ -1,10 +1,11 @@
 <template>
   <li class="cards-list__item d-inline-block">
     <div class="small-card text-left" v-bind:class="{'small-card--animate' : animate}">
-      <div class="small-card__type card-type" v-bind:class="['card-type--' + ((cardPan && cardPan.length >= 1) ? cardType : '')]"></div>
+      <div class="small-card__type card-type" v-bind:class="['card-type--' + ((pan && pan.length >= 1) ? cardType : '')]"></div>
       <div class="small-card__pan text-right">{{pan}}</div>
       <a href="#" class="small-card__delete" v-on:click.prevent="deleteCard"></a>
     </div>
+    <span class="text-muted small d-block text-center" v-if="info.is_default">По умолчанию</span>
   </li>
 </template>
 
@@ -14,15 +15,22 @@ import checkCard from '@/mixins/checkCard'
 export default {
   data () {
     return {
-      cardPan: this.pan
     }
   },
   mixins: [checkCard],
   props: {
-    pan: String,
+    info: {
+      type: Object,
+      default: () => {}
+    },
     animate: {
       type: Boolean,
       default: false
+    }
+  },
+  computed: {
+    pan () {
+      return this.info.pan
     }
   },
   mounted () {
