bed45dad84e00f3038178baa755ec22248fa9819
Maksim <pelmesh10@gmail.com>
2018-10-01 19:10:00 +0300

    maps key fix

--- src/store/gmaps-init.js
+++ src/store/gmaps-init.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
-const MAPS_KEY = 'AIzaSyBiUfkzIUxYQk9gF7ml5q5HEA-FGU7xqHE'
+const MAPS_KEY = 'AIzaSyAY2rfNMOeJkOQw8s33VzbL7_UBaSoaIes'
 
 Vue.use(VueGoogleMaps, {
-  load: {
-    key: MAPS_KEY,
-    libraries: 'places'
-  }
+    load: {
+        key: MAPS_KEY,
+        libraries: 'places'
+    }
 })
 
 export default VueGoogleMaps
